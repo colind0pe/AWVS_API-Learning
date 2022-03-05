1dd1b824fbc282a09d36049a22a88c418b14db41
colind0pe <1370227996@qq.com>
2022-03-05 11:44:22 +0800

    提交

--- awvs_api.py
+++ awvs_api.py
@@ -25,10 +25,32 @@ def add_target():
     target_id = rep['target_id']
     return target_id
 
-def add_scan():
-    target_id = add_target()
+def config_target(target_id):
+    config_url = 'https://' + ip +':3443/api/v1/targets/' + target_id + '/configuration'
+    data = {
+            "description": "Target configuration default values",
+            "proxy": {
+                "enabled":"true","protocol":"http","address":"10.200.196.193","port":10809,"username":"","password":""
+            },
+            "sensor": "false",
+            "scan_speed": "fast",
+            }
+    data = bytes(json.dumps(data), 'utf-8')
+    r = requests.patch(url=config_url, headers=headers, data=data, verify=False)
+
+def add_scan(target_id):
     data = {"target_id": target_id, "profile_id": profile_id, "incremental": False,
                         "schedule": {"disable": False, "start_date": None, "time_sensitive": False}}
     data = bytes(json.dumps(data), 'utf-8')
     rep = requests.post(url=add_scan_url, headers=headers, data=data, verify=False).json()
-add_scan()
+
+def get_scans():
+    r = requests.get(url=add_scan_url, headers=headers, verify=False).json()
+    scan_id = r["scans"][0]['scan_id']
+    print(scan_id)
+
+def main():
+    target_id = add_target()
+    add_scan(target_id)
+    get_scans()
+main()
--- configure_target.py
+++ configure_target.py
@@ -6,7 +6,7 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-target_id = '3ca25d5e-fa76-4414-a5be-a0776bfba241'
+target_id = '3d195e64-0124-4118-b930-d85810025d9e'
 api_url = 'https://192.168.10.135:3443/api/v1/targets/' + target_id + '/configuration'
 headers = {
             'X-Auth': '1986ad8c0a5b3df4d7028d5f3c06e936c511f7e118d4e447cbf3e824950b5952b',
@@ -16,7 +16,7 @@ headers = {
 data = {
         "description": "Target configuration default values",
         "proxy": {
-            "enabled":"true","protocol":"http","address":"10.77.27.94","port":10809,"username":"","password":""
+            "enabled":"true","protocol":"http","address":"10.200.196.193","port":10809,"username":"","password":""
         },
         "sensor": "false",
         "scan_speed": "fast",
