4ac537b2b303f863d59753089ed46ee6233d897f
colind0pe <1370227996@qq.com>
2022-03-05 12:44:53 +0800

    提交

--- awvs_api.py
+++ awvs_api.py
@@ -9,8 +9,9 @@ ip = '192.168.10.135'
 api_url = 'https://' + ip + ':3443/api/v1'
 add_target_url = 'https://' + ip + ':3443/api/v1/targets'
 add_scan_url = 'https://' + ip +':3443/api/v1/scans'
-profile_id= '11111111-1111-1111-1111-111111111112'
-target_address = 'http://testphp.vulnweb.com/'
+get_vul_url = 'https://' + ip + ':3443/api/v1/vulnerabilities'
+profile_id= '11111111-1111-1111-1111-111111111111'
+# target_address = 'http://testphp.vulnweb.com/'
 
 headers = {
     'X-Auth': '1986ad8c0a5b3df4d7028d5f3c06e936c511f7e118d4e447cbf3e824950b5952b',
@@ -19,6 +20,7 @@ headers = {
 }
 
 def add_target():
+    target_address = input('输入目标地址:') 
     data = {"address": target_address,"description": "test","type": "default","criticality": 30}
     data = bytes(json.dumps(data), 'utf-8')
     rep = requests.post(url=add_target_url, headers=headers, data=data, verify=False).json()
@@ -47,10 +49,18 @@ def add_scan(target_id):
 def get_scans():
     r = requests.get(url=add_scan_url, headers=headers, verify=False).json()
     scan_id = r["scans"][0]['scan_id']
-    print(scan_id)
+    return scan_id
+
+def get_vul():
+    r = requests.get(url=get_vul_url, headers=headers, verify=False).json()
+    for vuln in r['vulnerabilities']:
+            print('漏洞名称:',vuln['vt_name'],',危险等级:',vuln['severity'])
 
 def main():
-    target_id = add_target()
-    add_scan(target_id)
-    get_scans()
+    # target_id = add_target()
+    # config_target(target_id)
+    # add_scan(target_id)
+    # scan_id = get_scans()
+    # print (scan_id)
+    get_vul()
 main()
